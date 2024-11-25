67a983e9d52915a85553ebbf24076abd24b386a7
TAMIL SELVAN <tamilnaresh62@gmail.com>
2024-11-25 12:30:34 +0530

    new

--- src/pages/weather.js
+++ src/pages/weather.js
@@ -1,8 +1,12 @@
 import './style.css'
+import './assets/tree.jpg';
 function Weather()  {
     return (
-             <div className="container1">
-                
+             <div className="body">
+                <div className='container1'>
+                    hello world
+                </div>
+            
              </div>
     );
 } 
