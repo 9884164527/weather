33b7192ce977467ca5d13bbc40f84c4829a1beac
TAMIL SELVAN <tamilnaresh62@gmail.com>
2024-12-10 10:47:27 +0530

    new

--- src/pages/weather.js
+++ src/pages/weather.js
@@ -77,7 +77,7 @@ const Weather = () => {
             <h1>Weather App</h1>
 
           </div>
-          <p>Get the current weather and 3hrs forecast for any city</p>
+          <p>Get the current weather and 3hr forecast for any city</p>
           
 
         </div>
